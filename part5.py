d270a025a39c9ae4b06726984f46d048c8506169
Skye Papa <94186676+skyepapa@users.noreply.github.com>
2021-12-11 18:16:56 +0000

    i finished the assignment

--- part5.py
+++ part5.py
@@ -18,3 +18,13 @@ Enter a number or enter 0 to stop: 0
 Number of evens: 3
 
 '''
+evens = 0
+
+num = int(input("Enter a number or 0 to stop: "))
+
+while num != 0:
+  if num % 2 == 0:
+    evens += 1
+  num = int(input("Enter a number or 0 to stop: "))
+
+print("Number of evens: " + str(evens))
